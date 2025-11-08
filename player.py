7a145190639e5c83728011cb6cae819345a7bea4
dr-wow-4U <juliew826@protonmail.com>
2025-11-08 14:25:39 -0500

    thesetoo2222day

---
+++ player.py
@@ -0,0 +1,3 @@
+The Player constructor should take x and y integers as input, then:
+Call the parent class's constructor, passing in x, y, and the PLAYER_RADIUS (you'll need to import it from your constants)
+Create an attribute called rotation, initialized to 0.
